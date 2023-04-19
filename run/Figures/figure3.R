6e1ce018174e3543ab38dd239b36b611dc4a1999
David <2306427+dhelekal@users.noreply.github.com>
2023-04-19 14:22:32 +0100

    update fig3

--- run/Figures/figure3.R
+++ run/Figures/figure3.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(patchwork)
 p1 <- plot_epi_dynamic(out, 2) +
-    scale_x_continuous(breaks = seq(t0, tmax, by =1)) +
+    scale_x_continuous(breaks = seq(t0, tmax, by =2)) +
     ggtitle("Resistant Lineage 1")
 p1 <- p1 + (p1$theme %+replace% theme(legend.position="none",
     axis.title.x=element_blank(),
@@ -9,7 +9,7 @@ p1 <- p1 + (p1$theme %+replace% theme(legend.position="none",
     axis.ticks.x=element_blank()))
 
 p2 <- plot_epi_dynamic(out, 3) +
-    scale_x_continuous(breaks = seq(t0, tmax, by =1)) +
+    scale_x_continuous(breaks = seq(t0, tmax, by =2)) +
     ggtitle("Resistant Lineage 2")
 p2 <- p2 + (p2$theme %+replace% theme(axis.title.y=element_blank(),
     axis.text.y=element_blank(),
@@ -26,17 +26,17 @@ p2 <- p2+ylim(y1_min,y1_max)
 
 
 p3 <- plot_rt(out, 2) +
-    scale_x_continuous(breaks = seq(t0, tmax, by =1))
+    scale_x_continuous(breaks = seq(t0, tmax, by =2))
 p3 <- p3 + (p3$theme %+replace% theme(legend.position="none",plot.title=element_blank(),
-    axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5)))
+    axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5))) 
 
-p4 <- plot_rt(out, 2) +
-    scale_x_continuous(breaks = seq(t0, tmax, by =1))
+p4 <- plot_rt(out, 3) +
+    scale_x_continuous(breaks = seq(t0, tmax, by =2))
 p4 <- p4 + (p4$theme %+replace% theme(axis.title.y=element_blank(),
     axis.text.y=element_blank(),
     axis.ticks.y=element_blank(),
     plot.title=element_blank(),
-    axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5)))
+    axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5))) 
 
 fig3 <- (p1 | p2)/(p3 | p4)
 
